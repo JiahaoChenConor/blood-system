a0a425bc4ad48cbe60180b44503561d1acb50ba9
ivankosenko <kosenko.bmstu@gmail.com>
2022-10-02 17:53:44 +1100

    sending SMS works

--- src/main/java/com/elec5619/bloodsystem/controller/RequestController.java
+++ src/main/java/com/elec5619/bloodsystem/controller/RequestController.java
@@ -5,6 +5,7 @@ import com.elec5619.bloodsystem.service.AccountService;
 import com.elec5619.bloodsystem.service.EmailService;
 import com.elec5619.bloodsystem.service.HistoryRecordService;
 import com.elec5619.bloodsystem.service.MessageRecordService;
+import com.elec5619.bloodsystem.service.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -154,6 +155,10 @@ public class RequestController {
             // save to db.
             request.setMatched(false);
             request.setContent(message);
+
+            //send sms
+            System.out.println("sending sms");
+            SmsService.sendSMS(message);
             // find matchers
             List<HistoryRecord> donates = historyRecordService.getMatchDonateRecord(request.getBloodType());
 
--- src/main/java/com/elec5619/bloodsystem/service/SmsService.java
+++ src/main/java/com/elec5619/bloodsystem/service/SmsService.java
@@ -1,5 +1,31 @@
 package com.elec5619.bloodsystem.service;
 
-public class SmsService {
+import com.twilio.Twilio;
+import com.twilio.converter.Promoter;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 
+import java.net.URI;
+import java.math.BigDecimal;
+
+public class SmsService {
+  // Find your Account Sid and Token at twilio.com/console
+  public static final String ACCOUNT_SID =
+      System.getenv("TWILIO_API_SID") == null
+          ? "AC665ef3544181cad0a9f94e83992131d4"
+          : System.getenv("TWILIO_API_SID");
+  public static final String AUTH_TOKEN =
+      System.getenv("TWILIO_API_KEY") == null
+          ? "7a89e554e8564c41a71a2b83cff9c3aa"
+          : System.getenv("TWILIO_API_KEY");
+  public static void sendSMS( String smstext) {
+    Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+    Message message = Message.creator(
+            new com.twilio.type.PhoneNumber("+61415548977"),
+            "MGf03805d894df253688c4b3b43e6b4927",
+            smstext)
+        .create();
+
+    System.out.println(message.getSid());
+  }
 }
